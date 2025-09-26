ebdad92bd1b172f99f9827d7775975d5c69075a2
Gegi Ghvachliani <gegighvachliani@Gegis-MacBook-Pro.local>
2025-09-26 20:50:45 +0400

    add empty project

---
+++ Assignment 11/Assignment11/Assignment11/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment11
+//
+//  Created by Gegi Ghvachliani on 26.09.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
