be021b0fd8643d656c3b10ef57caf72d64052103
Gegi Ghvachliani <gegighvachliani@Gegis-MacBook-Pro.local>
2025-10-02 19:52:50 +0400

    improved Dark/Light mode. added shadow effect on equal button

--- Assignment 12/Assignment12/Assignment12/ViewController.swift
+++ Assignment 12/Assignment12/Assignment12/ViewController.swift
@@ -8,13 +8,23 @@
 import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var moonSunButton: UIButton!
+    @IBOutlet weak var equalButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        equalButtonConfig()
     }
     
     @IBAction func moonSunButtonAction(_ sender: Any) {
         overrideUserInterfaceStyle = (overrideUserInterfaceStyle == .dark) ? .light : .dark
+
+        let imageName = (overrideUserInterfaceStyle == .dark) ? "sun.max" : "moon"
+            moonSunButton.setImage(UIImage(systemName: imageName), for: .normal)
+    }
+    private func equalButtonConfig() {
+        equalButton.layer.shadowColor = UIColor.systemPink.cgColor
+        equalButton.layer.shadowOpacity = 0.7
+        equalButton.layer.shadowOffset = CGSize(width: 2, height: 2)
+        equalButton.layer.shadowRadius = 10
     }
 }
-
