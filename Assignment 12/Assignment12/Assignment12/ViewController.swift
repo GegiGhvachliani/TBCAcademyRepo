9d979c175b587b81fd88a27ace3acb4c116c1c40
Gegi Ghvachliani <gegighvachliani@Gegis-MacBook-Pro.local>
2025-10-02 16:58:44 +0400

    made code little better. that is not my final, i will make some adjusements ASAP until you'll view my assignment. (gradient collor, dark/light mode)

--- Assignment 12/Assignment12/Assignment12/ViewController.swift
+++ Assignment 12/Assignment12/Assignment12/ViewController.swift
@@ -7,9 +7,14 @@
 
 import UIKit
 class ViewController: UIViewController {
-
+    @IBOutlet weak var moonSunButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func moonSunButtonAction(_ sender: Any) {
+        overrideUserInterfaceStyle = (overrideUserInterfaceStyle == .dark) ? .light : .dark
     }
 }
 
