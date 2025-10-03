d312469851f360f95f464a179d9feb382a34ed59
Gegi Ghvachliani <gegighvachliani@Gegis-MacBook-Pro.local>
2025-10-03 21:30:21 +0400

    add empty project

---
+++ Assignment 13/Assignment13/Assignment13/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  Assignment13
+//
+//  Created by Gegi Ghvachliani on 03.10.25.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let scene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: scene)
+        let vc = ProfileVC()
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+    }
+}
+
