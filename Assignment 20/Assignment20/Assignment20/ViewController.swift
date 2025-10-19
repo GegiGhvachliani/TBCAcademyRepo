c57e862b10d6352d03dbb10a180ece44dafe13f2
Gegi Ghvachliani <gegighvachliani@Gegis-MacBook-Pro.local>
2025-10-20 03:22:14 +0400

    add Assignment 20 emprt project

---
+++ Assignment 20/Assignment20/Assignment20/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment20
+//
+//  Created by Gegi Ghvachliani on 20.10.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
