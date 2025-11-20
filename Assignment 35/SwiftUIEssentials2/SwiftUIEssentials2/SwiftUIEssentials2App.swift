8fe62312501ccdf4ac0f27c9a296f86ca303e35d
Gegi Ghvachliani <GegiGhvachliani@gmail.com>
2025-11-21 01:28:28 +0400

    add initial project

---
+++ Assignment 35/SwiftUIEssentials2/SwiftUIEssentials2/SwiftUIEssentials2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIEssentials2App.swift
+//  SwiftUIEssentials2
+//
+//  Created by Gegi Ghvachliani on 21.11.25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIEssentials2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
