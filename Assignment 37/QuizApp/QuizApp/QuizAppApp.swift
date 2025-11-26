657ea2e521d0ee6643b51ca00dc96697032823f9
Gegi Ghvachliani <GegiGhvachliani@gmail.com>
2025-11-26 21:39:34 +0400

    add QuizApp initial project

---
+++ Assignment 37/QuizApp/QuizApp/QuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAppApp.swift
+//  QuizApp
+//
+//  Created by Gegi Ghvachliani on 26.11.25.
+//
+
+import SwiftUI
+
+@main
+struct QuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
