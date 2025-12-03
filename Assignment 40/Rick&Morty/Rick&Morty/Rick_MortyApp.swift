bbcf15d3d8891c787ce706b19695e128a2c61a85
Gegi Ghvachliani <GegiGhvachliani@gmail.com>
2025-12-03 23:20:57 +0400

    Add initial project

---
+++ Assignment 40/Rick&Morty/Rick&Morty/Rick_MortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_MortyApp.swift
+//  Rick&Morty
+//
+//  Created by Gegi Ghvachliani on 03.12.25.
+//
+
+import SwiftUI
+
+@main
+struct Rick_MortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
