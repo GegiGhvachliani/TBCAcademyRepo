2250c4adafccda8410ce71a8e27aeca91b0ccb7d
Gegi Ghvachliani <GegiGhvachliani@gmail.com>
2025-12-11 02:43:33 +0400

    Add Assignment 43 initial project

---
+++ Assignment 43/MusicsApp/MusicsApp/MusicsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicsAppApp.swift
+//  MusicsApp
+//
+//  Created by Gegi Ghvachliani on 11.12.25.
+//
+
+import SwiftUI
+
+@main
+struct MusicsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
